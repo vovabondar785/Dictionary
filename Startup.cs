f927404b6c1d18fcba3859a050e994ddd206a454
vovabondar785 <vovabondar785@gmail.com>
2020-02-09 20:34:12 +0200

    3

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,12 @@ namespace Dictionary
 {
     public class Startup
     {
+        private IConfigurationRoot conf;
+
+        public Startup(IWebHostEnvironment host)
+        {
+            conf = new ConfigurationBuilder().SetBasePath(host.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
