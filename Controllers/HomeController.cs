a7968facdf3e20dbf62957ee7128ab4e37c99899
Vladyslavvs <57898919+Vladyslavvs@users.noreply.github.com>
2020-02-09 20:06:11 +0200

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Dictionary.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult HomePage()
         {
             return View();
         }
